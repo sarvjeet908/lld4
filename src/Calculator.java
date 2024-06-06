3a8537a1fda773d64c0e01c9617886cce18cc5e4
sarvjeet <sarvjeet908@gmail.com>
2024-06-06 17:54:47 +0530

    interest calculator added

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("demo for merge this branch with master branch");g
+        System.out.println("demo for merge this branch with master branch");
     }
 }
